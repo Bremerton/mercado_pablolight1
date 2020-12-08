edb354cacea0ae5b39971022ef5b7f4c11d4ae76
Bremerton <72746036+Bremerton@users.noreply.github.com>
2020-12-08 10:46:42 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+x = input.light_level()
+while True:
+    print("Light Level:" + input.light_level())
+    if x > 128:
+        light.set_all(#ff0000)
---
+++ main.ts
@@ -0,0 +1,8 @@
+let x = input.lightLevel()
+while (true) {
+    console.log("Light Level:" + input.lightLevel())
+    if (x > 128) {
+        light.setAll
+    }
+    
+}
